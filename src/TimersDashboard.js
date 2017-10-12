e932d7e4ed61bf458dfa622a4e04bce972c913a9
abadia2 <abadia2@nationwide.com>
2017-10-12 14:25:14 -0400

    added timer state to timerDashboard

--- src/TimersDashboard.js
+++ src/TimersDashboard.js
@@ -1,13 +1,32 @@
 import React,{Component} from 'react';
+import uuid from 'uuid';
 import EditableTimerList from "./EditableTimerList";
 import ToggleableTimerForm from './ToggleableTimerForm';
 
 class TimersDashboard extends Component{
+    state={
+        timers:[
+            {
+                title:"Practice squate",
+                project: 'Gym Chores',
+                id: uuid.v4(),
+                elapsed:5456099,
+                runningSince:Date.now()
+            },
+            {
+                title:'Bake squash',
+                project:'Kitchen Chores',
+                id:uuid.v4(),
+                elapsed:1273998,
+                runningSince:null
+            }
+        ]
+    };
     render(){
         return(
             <div className="ui three column centered grid">
                 <div className="column">
-                    <EditableTimerList/>
+                    <EditableTimerList timers={this.state.timers}/>
                     <ToggleableTimerForm isOpen={false}/>
                 </div>
             </div>
